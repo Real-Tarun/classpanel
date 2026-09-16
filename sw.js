93e9ad258c59815963a54583a343178bd2c67f50
Tarun <tarun.pncml123@gmail.com>
2026-09-16 10:38:15 +0530

    Install icons for Clocks, Random Number Generator, and Tally Counter (12/12 tools complete)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'classpanel-v9';
+const CACHE_NAME = 'classpanel-v10';
 const STATIC_ASSETS = [
   '/',
   '/favicon.ico',
@@ -21,7 +21,10 @@ const STATIC_ASSETS = [
   '/assets/icons/group-generator.png',
   '/assets/icons/exam-timer.png',
   '/assets/icons/sensory-timer.png',
+  '/assets/icons/clocks.png',
+  '/assets/icons/random-number-generator.png',
   '/assets/icons/chance-games.png',
+  '/assets/icons/tally-counter.png',
   '/assets/icons/presentation-timer.png',
   '/assets/icons/race-timers.png',
   '/assets/icons/holiday-timers.png',
